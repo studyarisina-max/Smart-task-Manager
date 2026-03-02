ec79a469f698c2d1ab0f39997f2254908ce40fec
Arisina <studyarisina@gmail.com>
2026-03-02 19:43:09 +0530

    Update api.ts

--- frontened/api.ts
+++ frontened/api.ts
@@ -4,10 +4,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const API = axios.create({
 
   baseURL: 'https://internship-11-ju1d.onrender.com/api',
-  timeout: 10000, 
+  timeout: 15000, 
 });
 
-// Automatically add the token to every request
+
 API.interceptors.request.use(async (config) => {
   try {
     const token = await AsyncStorage.getItem('token');
